16ea4b27accd89c2220dd8c4093ee519b0cd8488
trungvinh02 <work.trungvinh@gmail.com>
2024-06-20 10:31:53 +0700

    Update

--- config/app.js
+++ config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.SERVER_PORT,
+    port: process.env.SERVER_PORT || 8000,
     prefixApiVersion: process.env.PREFIX_API_VERSION || "/api/v1",
     jwtAccessKey: process.env.JWT_ACCESS_KEY || "test",
 };
